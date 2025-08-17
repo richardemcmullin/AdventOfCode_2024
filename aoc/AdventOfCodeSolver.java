15f85f7af68bb902d1a13f436ac3c50d1e5c4403
Richard McMullin <richard.e.mcmullin@gmail.com>
2025-08-17 12:22:01 -0400

    AdventOfCodeSolver

---
+++ aoc/AdventOfCodeSolver.java
@@ -0,0 +1,51 @@
+package aoc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public abstract class AdventOfCodeSolver {
+    protected List<String> inputLines;
+
+    private void readInput() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            inputLines = reader.lines().toList();
+        } catch (Exception e) {
+            throw new RuntimeException("Error reading input", e);
+        }
+    }
+
+    /**
+     * Parse the input lines into the required data structures.
+     */
+    protected abstract void parseInput();
+
+    /**
+     * Solve part 1 of the puzzle.
+     */
+    public abstract Object solvePart1();
+
+    /**
+     * Solve part 2 of the puzzle.
+     */
+    public abstract Object solvePart2();
+
+    public void run() {
+
+        readInput();
+        parseInput();
+
+        long start1 = System.nanoTime();
+        Object part1 = solvePart1();
+        long end1 = System.nanoTime();
+        long time1 = Math.round((end1 - start1) / 1_000_000.0);
+        System.out.println("Part 1: " + part1 + " (" + time1 + " ms)");
+
+        long start2 = System.nanoTime();
+        Object part2 = solvePart2();
+        long end2 = System.nanoTime();
+        long time2 = Math.round((end2 - start2) / 1_000_000.0);
+        System.out.println("Part 2: " + part2 + " (" + time2 + " ms)");
+    }
+}
